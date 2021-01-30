6c859e05d875db0911c61fdc243d040d2b65e46e
tom <tommy.dumont@uqtr.ca>
2021-01-30 17:48:13 -0500

    routing vers detail fait

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DetailsComponent} from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details/:idx',  component: DetailsComponent }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
