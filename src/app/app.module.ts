31a361e5dd0ab516987b57c503bec1583b2c5b2a
julien98Qc <julien_turcotte@hotmail.fr>
2021-03-23 15:13:15 -0400

    Module divisé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,108 +10,25 @@ import {IndividuService} from './services/individu.service';
 
 // Composants dans notre service
 import { AppComponent } from './app.component';
-import { DetailsComponent } from './details/details.component';
-import { EditorComponent } from './editor/editor.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { ListeIndividuComponent } from './liste-individu/liste-individu.component';
-import { FormComponent } from './form/form.component';
 
-// Module pour les formulaires
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
-import { LayoutModule } from '@angular/cdk/layout';
-// Angular Material Components
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatInputModule} from '@angular/material/input';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
 import {AuthAccessGuard} from './login/auth-access.guard';
-// import {RouterModule} from '@angular/router';
+import {FormGridDisplayModule} from './form-grid-display/form-grid-display.module';
+import {ListeIndividuComponent} from './liste-individu/liste-individu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent,
-    EditorComponent,
     PageNotFoundComponent,
     NavMenuComponent,
-    FormComponent,
-    ListeIndividuComponent
+    ListeIndividuComponent,
   ],
   imports: [
     BrowserAnimationsModule,
-    MatSidenavModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatMenuModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatStepperModule,
-    MatTabsModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatChipsModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatTooltipModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-    FormsModule,
     AppRoutingModule,
+    FormGridDisplayModule,
   ],
   providers: [
     IndividuService,
